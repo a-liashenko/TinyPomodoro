af3a43a5960fad915151beb50e40a7b305358b9a
Aleksandr Liashenko <icealexxx@gmail.com>
2022-01-14 23:48:01 +0300

    Rename old titlebar to topbar

---
+++ src/app/components/topbar.rs
@@ -0,0 +1,27 @@
+use super::{AppComponent, UIPages};
+use crate::app::{widgets::IconButton, App};
+use eframe::egui::Layout;
+
+pub struct Topbar;
+
+impl AppComponent for Topbar {
+    type Context = App;
+
+    fn add(ctx: &mut Self::Context, ui: &mut eframe::egui::Ui) {
+        ui.horizontal(|ui| {
+            ui.with_layout(Layout::right_to_left(), |ui| {
+                ui.add_space(15.0);
+
+                let settings = IconButton::new(&ctx.resources.icons().settings);
+                if ui.add(settings).clicked() {
+                    ctx.page = if ctx.page == UIPages::Main {
+                        UIPages::Settings
+                    } else {
+                        ctx.pomodoro.update_config(ctx.config.pomodoro);
+                        UIPages::Main
+                    }
+                }
+            });
+        });
+    }
+}

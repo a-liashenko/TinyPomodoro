530b9acf058b77b03276af63058a67fde583c0b1
Aleksandr Liashenko <icealexxx@gmail.com>
2022-07-31 23:51:16 +0400

    Final fix for all fonts

--- app/src/resources/fonts.rs
+++ app/src/resources/fonts.rs
@@ -1,9 +1,10 @@
 use crate::defines::fonts::{FONT_ROBOTO, FONT_ROBOTO_MONO};
-use eframe::egui::{Context, FontData, FontDefinitions, Style, TextStyle};
+use eframe::egui::{FontData, FontDefinitions, Style, TextStyle};
 use eframe::epaint::FontId;
 
 pub fn register_fonts(style: &mut Style) {
     use eframe::epaint::FontFamily::{Monospace, Proportional};
+
     style.text_styles = [
         (TextStyle::Body, FontId::new(21.0, Proportional)),
         (TextStyle::Button, FontId::new(18.0, Monospace)),
@@ -15,12 +16,17 @@ pub fn register_fonts(style: &mut Style) {
 }
 
 pub fn load_fonts() -> FontDefinitions {
+    use eframe::epaint::FontFamily::{Monospace, Proportional};
+
     let mut fonts = FontDefinitions::default();
     let roboto = FontData::from_static(FONT_ROBOTO);
     let roboto_mono = FontData::from_static(FONT_ROBOTO_MONO);
 
     fonts.font_data.insert("roboto".into(), roboto);
     fonts.font_data.insert("roboto_mono".into(), roboto_mono);
+
+    fonts.families.insert(Monospace, vec!["roboto_mono".into()]);
+    fonts.families.insert(Proportional, vec!["roboto".into()]);
 
     fonts
 }
